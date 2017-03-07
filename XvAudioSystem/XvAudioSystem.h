83f8fe89ac466351145387402b930c9a24a3e66e
jasonjsnell <snell.jason@gmail>
2017-03-07 20:18:08 +0100

    Initial Commit

---
+++ XvAudioSystem/XvAudioSystem.h
@@ -0,0 +1,19 @@
+//
+//  XvAudioSystem.h
+//  XvAudioSystem
+//
+//  Created by Jason Snell on 3/7/17.
+//  Copyright © 2017 Jason J. Snell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XvAudioSystem.
+FOUNDATION_EXPORT double XvAudioSystemVersionNumber;
+
+//! Project version string for XvAudioSystem.
+FOUNDATION_EXPORT const unsigned char XvAudioSystemVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XvAudioSystem/PublicHeader.h>
+
+
